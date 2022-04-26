38f8176c023c1d661fca7603fc5b7799eda38189
Kazuki Arai <zukain5@gmail.com>
2022-04-26 16:09:43 +0900

    add calc_eq

--- main.c
+++ main.c
@@ -1,3 +1,37 @@
+#include <stdio.h>
+#define N 100
+
+void calc_eq(double eq[][N+1], int NE, double L, double Q, double A, double lambda){
+    for (int i=0; i<NE; i++){
+        // K
+        double k_abs = lambda * A / L;
+        eq[i][i] += k_abs;
+        eq[i][i+1] -= k_abs;
+        eq[i+1][i] -= k_abs;
+        eq[i+1][i+1] += k_abs;
+        
+        // f
+        double f_abs = Q * A * L / 2;
+        eq[i][NE+1] += f_abs;
+        eq[i+1][NE+1] += f_abs;
+    }
+}
+
+void solve_equations(double eq[][N+1], int NE){
+
+}
+
 int main(void){
+    // 入力
+    int NE = 4;
+    double L = 1.0;
+    double Q = 1.0;
+    double A = 1.0;
+    double lambda = 1.0;
+
+    double eq[N][N+1] = {0};
+    calc_eq(eq, NE, L, Q, A, lambda);
+    solve_equations(eq, NE);
+
     return 0;
 }
