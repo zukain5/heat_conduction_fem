d0aff00d6047cba4e06c2fc4a7b008f62eda1c7f
Kazuki Arai <zukain5@gmail.com>
2022-05-19 12:02:15 +0900

    テストを修正

--- Makefile
+++ Makefile
@@ -1,13 +1,22 @@
-TARGET      = simulate.out
-SRCS        = 1dim_fem.c utility.c simulate.c
-OBJS        = $(SRCS:.c=.o)
-CC          = gcc
-CFLAG       = -Wall
+TARGET      	= simulate.out
+TEST_TARGET		= test.out
+SRCS        	= 1dim_fem.c utility.c simulate.c
+TEST_SRCS		= test.cpp 1dim_fem.c
+OBJS        	= $(SRCS:.c=.o)
+TEST_OBJS		= $(TEST_SRCS:.c=.o)
+CC          	= gcc
+CFLAG       	= -Wall
+PP				= clang++
+CPPFLAGS		= -I$(CPPUTEST_HOME)/include
+LD_LIBRARIES	= -L$(CPPUTEST_HOME)/lib -lCppUTest -lCppUTestExt
 
 $(TARGET):  $(OBJS)
 			$(CC) $(OBJS) -o $(TARGET)
 
-all:        $(TARGET)
+test:		$(TEST_OBJS)
+			$(PP) $(TEST_OBJS) -o $(TEST_TARGET) $(CPPFLAGS) $(LD_LIBRARIES)
+
+all:        $(TARGET) test
 
 .c.o:
 			$(CC) -c $< -o $@
@@ -17,5 +26,6 @@ clean:
 
 fclean:     clean
 			rm -f $(TARGET)
+			rm -f $(TEST_TARGET)
 
 re:         fclean all
--- test.cpp
+++ test.cpp
@@ -3,6 +3,7 @@
 #include "1dim_fem.h"
 
 double eq[N][N+1]={0};
+double ans[N+1]={0};
 int NE=2;
 int n=3;
 double TOLERANCE = 0.0001;
@@ -25,9 +26,9 @@ TEST_GROUP(TestFuncGroup){
 };
 
 TEST(TestFuncGroup, internal_functions){
-    int pivot_i = select_pivot(eq, n, 0);
+    int pivot_i = _select_pivot(eq, n, 0);
     CHECK_EQUAL(2, pivot_i);
-    swap_row(eq, n, pivot_i, 0);
+    _swap_row(eq, n, pivot_i, 0);
     DOUBLES_EQUAL(3  ,eq[0][0], TOLERANCE);
     DOUBLES_EQUAL(-1 ,eq[0][1], TOLERANCE);
     DOUBLES_EQUAL(2  ,eq[0][2], TOLERANCE);
@@ -40,7 +41,7 @@ TEST(TestFuncGroup, internal_functions){
     DOUBLES_EQUAL(1  ,eq[2][1], TOLERANCE);
     DOUBLES_EQUAL(1  ,eq[2][2], TOLERANCE);
     DOUBLES_EQUAL(6  ,eq[2][3], TOLERANCE);
-    normalize_row(eq, n, 0);
+    _normalize_row(eq, n, 0);
     DOUBLES_EQUAL(1     ,eq[0][0], TOLERANCE);
     DOUBLES_EQUAL(-1./3 ,eq[0][1], TOLERANCE);
     DOUBLES_EQUAL(2./3  ,eq[0][2], TOLERANCE);
@@ -53,7 +54,7 @@ TEST(TestFuncGroup, internal_functions){
     DOUBLES_EQUAL(1     ,eq[2][1], TOLERANCE);
     DOUBLES_EQUAL(1     ,eq[2][2], TOLERANCE);
     DOUBLES_EQUAL(6     ,eq[2][3], TOLERANCE);  
-    subtract_row(eq, n, 0);
+    _subtract_row(eq, n, 0);
     DOUBLES_EQUAL(1     ,eq[0][0], TOLERANCE);
     DOUBLES_EQUAL(-1./3 ,eq[0][1], TOLERANCE);
     DOUBLES_EQUAL(2./3  ,eq[0][2], TOLERANCE);
@@ -69,7 +70,7 @@ TEST(TestFuncGroup, internal_functions){
 }
 
 TEST (TestFuncGroup, solve_equations){
-    solve_equations(eq, n);
+    _solve_equations(eq, n, ans);
     DOUBLES_EQUAL(1 ,eq[0][0], TOLERANCE);
     DOUBLES_EQUAL(0 ,eq[0][1], TOLERANCE);
     DOUBLES_EQUAL(0 ,eq[0][2], TOLERANCE);
@@ -83,6 +84,8 @@ TEST (TestFuncGroup, solve_equations){
     DOUBLES_EQUAL(1 ,eq[2][2], TOLERANCE);
     DOUBLES_EQUAL(3 ,eq[2][3], TOLERANCE);     
 }
+
+
 
 int main(int ac, char** av){
     return CommandLineTestRunner::RunAllTests(ac, av);
