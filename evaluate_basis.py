2a0790b69dc37cc55c1aeb2975c90d8fa1ea5654
zukain5 <zukain5@gmail.com>
2022-05-19 15:24:45 +0900

    evaluate_basisが正しく動くように修正

--- evaluate_basis.py
+++ evaluate_basis.py
@@ -3,14 +3,16 @@ import numpy as np
 
 def main():
     # 基底の読み込み
-    basis_count = 3
-    n = 301
+    with open('PARAMETERS') as f:
+        info_l = f.readlines()
+    basis_count = int(info_l[2])
+    n = int(info_l[0])
 
     with open('02_Vk') as f:
         basis_l = f.readlines()
 
-    basis = np.matrix(np.array(basis_l).reshape([basis_count, n]))
-    multiplier = ((basis * basis.T) ** -1) * basis.T
+    basis = np.matrix(np.array(basis_l, dtype=float).reshape([basis_count, n]))
+    multiplier = (((basis.T * basis) + 0.00001 * np.eye(n)) ** -1) * basis.T
 
     ys = np.loadtxt('OUTPUTALL.csv', delimiter=',')
 
